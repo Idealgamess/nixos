c13d1f90eefcb76a89763494bfa992e8d2565de1
Idealgamess <Zacharysharo@gmail.com>
2026-04-20 22:07:38 -0400

    updated config

--- flake.nix
+++ flake.nix
@@ -5,9 +5,12 @@
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    minesddm = {
+      url = "github:Davi-S/sddm-theme-minesddm/development";
+      flake = false;
+    };
   };
-
-  outputs = { nixpkgs, home-manager, ... }: {
+  outputs = { nixpkgs, home-manager, minesddm, ... }: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
@@ -18,6 +21,18 @@
           home-manager.useUserPackages = true;
           home-manager.users.ZacharyS = import ./home.nix;
         }
+        ({ pkgs, ... }: {
+          services.displayManager.sddm = {
+            enable = true;
+            theme = "minesddm";
+          };
+          environment.systemPackages = [
+            (pkgs.runCommand "sddm-theme-minesddm" {} ''
+              mkdir -p $out/share/sddm/themes
+              cp -r ${minesddm}/minesddm $out/share/sddm/themes/minesddm
+            '')
+          ];
+        })
       ];
     };
   };
